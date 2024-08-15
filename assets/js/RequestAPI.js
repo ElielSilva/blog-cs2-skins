a0a3e0740c1bbc3c0d7b2ffddb823ea58f787c01
Jadeilson Martiho <jadeilsonm17@gmail.com>
2024-08-15 14:16:02 -0300

    refactor: remove duplicate encoding

--- assets/js/RequestAPI.js
+++ assets/js/RequestAPI.js
@@ -1,22 +1,22 @@
 
+const URL_BASE = "https://pdmxsxggktrbgplirgvi.supabase.co/rest/v1";
+const HEADERS = {
+    "Content-Type":"application/json",
+    'apikey': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBkbXhzeGdna3RyYmdwbGlyZ3ZpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjE3NTU5MTUsImV4cCI6MjAzNzMzMTkxNX0.W02miXj37LQsKOwJPQxdfGbD5iXKuwsvE7fMt_Z6Juw',
+    'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBkbXhzeGdna3RyYmdwbGlyZ3ZpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjE3NTU5MTUsImV4cCI6MjAzNzMzMTkxNX0.W02miXj37LQsKOwJPQxdfGbD5iXKuwsvE7fMt_Z6Juw'
+}
+
 const requestAllSkins = async () => {
-  const URL = "https://pdmxsxggktrbgplirgvi.supabase.co/rest/v1/skins";
   try {
-    const response = await fetch(URL, {
+    const response = await fetch(URL_BASE + '/skins', {
         method: 'GET',
-        headers: {
-          "Content-Type":"application/json",
-          'apikey': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBkbXhzeGdna3RyYmdwbGlyZ3ZpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjE3NTU5MTUsImV4cCI6MjAzNzMzMTkxNX0.W02miXj37LQsKOwJPQxdfGbD5iXKuwsvE7fMt_Z6Juw',
-          'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBkbXhzeGdna3RyYmdwbGlyZ3ZpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjE3NTU5MTUsImV4cCI6MjAzNzMzMTkxNX0.W02miXj37LQsKOwJPQxdfGbD5iXKuwsvE7fMt_Z6Juw'
-        }
+        headers: HEADERS       
       }
     );
     if (!response.ok) {
       throw new Error(`Response status: ${response.status}`);
     }
     const json = await response.json()
-    console.log(json)
-    //window.arraySkins = Object.values(json)
     return json
 
   } catch (error) {
@@ -24,16 +24,27 @@ const requestAllSkins = async () => {
   }
 }
 
-const requestByIdWithComents = async (id) => {
-  const URL = "https://pdmxsxggktrbgplirgvi.supabase.co/rest/v1/rpc/get_joined_data";
+const createComent = async (data) => {
   try {
-    const response = await fetch(URL, {
+    const response = await fetch(URL_BASE + '/comments', {
         method: 'POST',
-        headers: {
-          "Content-Type":"application/json",
-          'apikey': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBkbXhzeGdna3RyYmdwbGlyZ3ZpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjE3NTU5MTUsImV4cCI6MjAzNzMzMTkxNX0.W02miXj37LQsKOwJPQxdfGbD5iXKuwsvE7fMt_Z6Juw',
-          'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBkbXhzeGdna3RyYmdwbGlyZ3ZpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjE3NTU5MTUsImV4cCI6MjAzNzMzMTkxNX0.W02miXj37LQsKOwJPQxdfGbD5iXKuwsvE7fMt_Z6Juw'
-        },
+        headers: HEADERS,
+        body: JSON.stringify(data)
+      }
+    );
+    if (!response.status === 201) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+  } catch (error) {
+      throw new Error(error.message);
+  }
+}
+
+const requestByIdWithComents = async (id) => {
+  try {
+    const response = await fetch(URL_BASE + '/rpc/get_joined_data', {
+        method: 'POST',
+        headers: HEADERS,
         body: JSON.stringify({ p_id: parseInt(id)})
       }
     );
@@ -41,39 +52,28 @@ const requestByIdWithComents = async (id) => {
       throw new Error(`Response status: ${response.status}`);
     }
     const json = await response.json()
-    console.log(json)
-    //window.arraySkins = Object.values(json)
     return json
-
   } catch (error) {
       console.error(error.message);
   }
 }
 
 const requestById = async (id) => {
-  const URL = `https://pdmxsxggktrbgplirgvi.supabase.co/rest/v1/skins?id=eq.${id}`;
   try {
-    const response = await fetch(URL, {
+    const response = await fetch(URL_BASE + `/skins?id=eq.${id}`, {
         method: 'GET',
-        headers: {
-          "Content-Type":"application/json",
-          'apikey': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBkbXhzeGdna3RyYmdwbGlyZ3ZpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjE3NTU5MTUsImV4cCI6MjAzNzMzMTkxNX0.W02miXj37LQsKOwJPQxdfGbD5iXKuwsvE7fMt_Z6Juw',
-          'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBkbXhzeGdna3RyYmdwbGlyZ3ZpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjE3NTU5MTUsImV4cCI6MjAzNzMzMTkxNX0.W02miXj37LQsKOwJPQxdfGbD5iXKuwsvE7fMt_Z6Juw'
-        },
+        headers: HEADERS
       }
     );
     if (!response.ok) {
       throw new Error(`Response status: ${response.status}`);
     }
     const json = await response.json()
-    console.log(json)
-    //window.arraySkins = Object.values(json)
     return json
-
   } catch (error) {
       console.error(error.message);
   }
 }
 
-window.utilsApi = { requestAllSkins, requestByIdWithComents, requestById }
+window.utilsApi = { requestAllSkins, requestByIdWithComents, requestById, createComent }
 
